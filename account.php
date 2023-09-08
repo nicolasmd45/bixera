6a4a7d190179c0f674e35043e89d211a3fbaf0bc
nibolau <88752542+nicolasmd45@users.noreply.github.com>
2023-09-08 13:47:33 -0300

    Add files via upload

--- account.php
+++ account.php
@@ -9,9 +9,9 @@ $conn = new mysqli($servername, $username, $password, "epiz_33958675_perfis");
 
 // checar conexão
 if ($conn->connect_error) {
-  die("falha na conexão: " . $conn->connect_error);
+  die("falha na conexão da conta: " . $conn->connect_error);
 }
-echo "conectado";
+echo "conta conectada";
 echo "<br>";
 
 ?>
